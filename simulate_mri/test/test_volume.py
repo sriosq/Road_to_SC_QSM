eb5e72aec82424b333465642ea10e623521e8bc3
Sebastian Rios <sebas.riosq99@gmail.com>
2024-06-18 01:47:40 -0400

    Update test folder codes

--- simulate_mri/test/test_volume.py
+++ simulate_mri/test/test_volume.py
@@ -7,4 +7,3 @@ def test_helloworld():
     img = nib.load(os.path.join(__dir_testing__, "final_sc_seg.nii.gz"))
     #assert
     # Use assert to verify that the output is GOOD not only that the code runs
-    
